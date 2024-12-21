d0f854d3979db72dc5087bc21e035105a9ce9052
Igor Taren <i.taren@andersenlab.com>
2024-12-21 11:23:03 +0300

    init commit

---
+++ src/main/java/my/project/step7gen/Step7GenApplication.java
@@ -0,0 +1,13 @@
+package my.project.step7gen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Step7GenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Step7GenApplication.class, args);
+    }
+
+}
